933cf4cdeb04a09677d4c1c62ffa2f9b7fc35b85
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-06-27 23:27:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def makeCommits (days : int):
         os.system('git add data.txt')
 
       
+        os.system('git commit --date="'+ dates +'" -m "cupertino library Null safety Flutter widgets implementing the current iOS design language."')
+
         return days * makeCommits(days - 1)
     
 makeCommits(3)
