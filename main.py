3e534fda06ceb1a355ed9b66253546db4271c556
rwema3 <rwemadominik3@gmail.com>
2023-08-13 00:13:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,3 @@ def makeCommits (days : int):
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- This was the commit for the !daY!!\n')
         
-        # Strangring with the m
