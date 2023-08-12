3a6251794b0f9717710b4346df12bffd9e4d73c1
rwema3 <rwemadominik3@gmail.com>
2023-08-12 23:59:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def makeCommits (days : int):
 
         os.system('git commit --date="'+ dates +'" -m "cupertino library"')
 
-        return days * makeCommits(days 
+        return days * makeCommits(days -
