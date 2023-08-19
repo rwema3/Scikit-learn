37c687611928d36f6f7957c06cb98079cacaeea4
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-08-19 18:29:14 +0200

    Update SpaceX.py

--- SpaceX.py
+++ SpaceX.py
@@ -7,5 +7,4 @@ class Computer:
     def config(self):
         print("Configuration is", self.name, self.age)
 
-com1 = Computer("Dominque", 21)
-com2 = Computer("Dominque", 31)
+com1 = Computer("Dominque", 
