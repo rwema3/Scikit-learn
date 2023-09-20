aef02d696405a6fc92cd7d086c969f1c8aaef31a
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-09-20 21:51:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ def makeCommits (days : int):
         dates = f"{days} days ago"
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- This was the commit for the !daY!!\n')
- 
+        
+]
