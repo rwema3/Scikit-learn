7bb04e9d92c308261a9544396e181aa58d8f8125
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-01 20:00:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ def makeCommits (days : int):
         
         # Strangring with the m
         os.system('git add data.txt')
+
+        os.system('git commit --date="'+ dates +'" -m "cupertino library"')
+
