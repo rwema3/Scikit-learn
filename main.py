1c7fb6e38671472e4d10ac05a76b95d166f600bf
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-12 00:02:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def makeCommits (days : int):
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- This was the commit for the !daY!!\n')
         
-        # Strangring
+        # Strangring 
         os.system('git add data.txt')
 
         os.system('git commit --date="'+ dates +'" -m "cupertino library"')
