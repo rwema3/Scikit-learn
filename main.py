eb81055295b8441acbe046b9c77c52fa79e42623
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-10-21 17:53:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,4 @@ def makeCommits (days : int):
         with open('data.txt', 'a') as file:
             file.write(f'{dates} <- This was the commit for the !daY!!\n')
         
-        # Strangring with the m
-        os.system('git add data.txt')
-
     
